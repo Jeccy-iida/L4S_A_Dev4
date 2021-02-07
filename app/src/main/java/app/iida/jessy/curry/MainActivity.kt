a1f3ae5bb340d001cbe7f1cbbcf9a9592caf7676
Jeccy-iida <drrr_drrr_4242@docomo.ne.jp>
2021-02-08 01:29:31 +0900

    初めてのコミット

---
+++ app/src/main/java/app/iida/jessy/curry/MainActivity.kt
@@ -0,0 +1,38 @@
+package app.iida.jessy.curry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlin.random.Random
+import android.widget.TextView
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    val people = arrayOf<String>("ちょき","ぐー","ぱー","ぷいぷい")
+    val food = arrayOf<String>("かれー","みそしる","ちゃーはん","えびちり")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun serveFood(view: View?){
+
+        val randomName = Random()
+        val index = randomName.nextInt(4)
+        val name = people  [index]
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val foodTextView = findViewById<TextView>(R.id.foodTextView)
+        val verbTextView = findViewById<TextView>(R.id.verbTextView)
+        nameTextView.text = name + "に"
+
+        foodTextView.text = food[Random().nextInt(4)]+"を"
+
+        if(Random().nextInt(100)<50){
+            verbTextView.text = "よそえました！"
+        }else{
+            verbTextView.text = "よそえませんでした"
+        }
+    }
+}
